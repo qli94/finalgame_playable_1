b5167c2adfadc2b7112817908ec38311fec9113a
qli94 <103575941+qli94@users.noreply.github.com>
2022-05-31 22:42:44 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 let config = {
     type: Phaser.CANVAS,
-    width: 840,
-    height: 525,
+    width: 1024,
+    height: 768,
     physics: {
         default: 'arcade',
         arcade: {
@@ -12,12 +12,12 @@ let config = {
             }
         }
     },
-    scene: [Menu, Play, Details, Rules]
+    scene: [Scene1, Transition1, Scene2, Transition2, Scene3]
 };
 
 let game = new Phaser.Game(config);
 
-let keyUP, keyRIGHT, keyLEFT, keySPACE, keyR, keyDOWN, keyF;
+let keyUP, keyRIGHT, keyLEFT, keySPACE, keyR, keyDOWN, keyF; keyRIGHT;
 
 const SCALE = 0.5;
 
