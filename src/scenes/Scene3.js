cb8010adc7e7f8cb1df766aa1a89893f3e98db1b
qli94 <103575941+qli94@users.noreply.github.com>
2022-06-03 01:38:57 +0800

    Update Scene3.js

--- src/scenes/Scene3.js
+++ src/scenes/Scene3.js
@@ -6,7 +6,7 @@ class Scene3 extends Phaser.Scene {
         this.load.path = "./assets/";
         this.load.image('3', 'scene3.png');
         this.load.image('player','player.png');
-        this.load.image('floor','floor.png');
+        this.load.image('platform','platform_center.png');
         this.load.image('monster','enemy.png');
         //this.load.image('T','m2.png');
         this.load.audio('jump', 'jump.wav');
@@ -37,7 +37,7 @@ class Scene3 extends Phaser.Scene {
         // setting values
         this.jumpSpeed = -1000;
         this.changedSpeed = 5;
-        this.speeding = 1;
+        this.speeding = 8;
         this.physics.world.gravity.y = 2600; 
 
         this.scene3 = this.add.tileSprite(0, 0, game.config.width, game.config.height, '3').setOrigin(0);
@@ -64,12 +64,6 @@ class Scene3 extends Phaser.Scene {
         // create monster(the big one)
         this.monster = this.physics.add.sprite(game.config.width, 455 ,  'monster').setOrigin(0);
         this.monster.body.allowGravity = false;
-        
-        
-
-        // monster2 (the smaller one)
-        //this.bat =  this.physics.add.sprite(game.config.width, Math.random()*(425-380)+380,  'bat').setOrigin(0);
-        //this.bat.body.allowGravity = false;
         
         // add enemies to group
         this.enemy = this.add.group();
@@ -219,11 +213,7 @@ class Scene3 extends Phaser.Scene {
             }
         }
         
-        //this.time1 = Math.trunc(15 - this.clock.getElapsedSeconds());
-       // this.timerRight.text = this.time1;
-
-        
-    }
+   
 
 
 
